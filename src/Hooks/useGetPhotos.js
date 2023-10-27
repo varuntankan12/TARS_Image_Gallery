6a49e33659e9478d6033f45f0b79bbccd3a93f04
varuntankan12 <varuntankan0080@gmail.com>
2023-10-27 17:52:12 +0530

    updated the data

---
+++ src/Hooks/useGetPhotos.js
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const accessKey = process.env.REACT_APP_CLIENT_ID;
+
+const useGetPhotos = () => {
+
+    const [responsedata, setResponseData] = useState([]);
+    const [searchresponsedata, setSearchResponseData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [issearcheddata, setIsearchedData] = useState(false);
+    const [prevsearch, setPrevSearch] = useState("no search");
+
+    const getPhotos = async (search) => {
+        setIsLoading(true);
+        try {
+            if (search !== "") {
+                const response = await axios.get(`https://api.unsplash.com/photos/random?query=${search}&count=18`, {
+                    headers: {
+                        Authorization: `Client-ID ${accessKey}`
+                    }
+                });
+                if (prevsearch === search) {
+                    setSearchResponseData(prev => [...prev, response.data]);
+                }
+                else {
+                    setSearchResponseData([response.data]);
+                    setPrevSearch(search);
+                }
+
+                setIsearchedData(true);
+            }
+            else {
+                const response = await axios.get(`https://api.unsplash.com/photos/random?count=18`, {
+                    headers: {
+                        Authorization: `Client-ID ${accessKey}`
+                    }
+                });
+                setResponseData(prev => [...prev, response.data]);
+                setIsearchedData(false);
+            }
+
+            setIsLoading(false);
+        } catch (error) {
+            console.error(error);
+            setIsLoading(false);
+            setResponseData(null);
+        }
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await getPhotos("");
+        };
+        fetchData();
+    }, []);
+
+    return [responsedata, searchresponsedata, isLoading, issearcheddata, getPhotos];
+}
+
+export default useGetPhotos;
